c5d44ec9ef45bbb4a1185685b061a3efc6316b63
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2025-11-18 21:26:41 +0400

    Constructed scheduler class

--- app/process-simulator/core/scheduler.ts
+++ app/process-simulator/core/scheduler.ts
@@ -2,21 +2,66 @@ import EventQueue from "./event-queue";
 import Process from "./process";
 
 export default class Scheduler {
-    readyQueue: Queue<Process> = new Queue();
-    runningProcess: Process | null = null;
+  readyQueue: Queue<Process> = new Queue();
+  runningProcess: Process | null = null;
+  mode: string;
 
-    selectNextProcess() {
-        return this.readyQueue.dequeue();
-    }
+  constructor(mode: string) {
+    this.mode = mode;
+  }
 
-    onProcessArrive(process: Process) {
-        process.state = "READY";
-        this.readyQueue.enqueue(process);
-    }
+  init(processList: Process[]) {
+    // Every process will start with a CPU burst not I/O. Therefore each of the processes
+    // are going to be in "READY" state.
 
-    startProcess(process: Process, currentTime: number) {
-        process.startCpuBurst();
-        this.runningProcess = process;
-        EventQueue.scheduleCpuDone(currentTime, process);
-    }
+    processList.forEach((p) => {
+      p.state = "READY";
+      this.readyQueue.enqueue(p);
+    });
+  }
+
+  onProcessArrive(process: Process) {
+    process.state = "READY";
+    this.readyQueue.enqueue(process);
+  }
+
+  onCpuFree() {
+    const next = this.selectNextProcess();
+    if(!next) return null;
+
+    this.runningProcess = next;
+    this.runningProcess.state = "RUNNING";
+    return this.runningProcess;
+  }
+
+  selectNextProcess() {
+    if (this.mode === "FCFS") return this.fcfs();
+    if (this.mode === "SJF") return this.sjf();
+    if (this.mode === "RR") return this.rr();
+  }
+
+  setProcessBlocked() {
+    if (!this.runningProcess) return null;
+
+    const p = this.runningProcess;
+    p.state = "BLOCKED";
+    this.runningProcess = null;
+    return p;
+  }
+
+  isCpuIdle() {
+    return this.runningProcess === null;
+  }
+
+  fcfs() {
+    return this.readyQueue.dequeue();
+  }
+
+  sjf() {
+    return null;
+  }
+
+  rr() {
+    return null;
+  }
 }
