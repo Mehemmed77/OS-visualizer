6c5df52e76394ede4a613a4d801002ab092f030f
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2025-11-26 23:29:28 +0400

    aa

--- app/process-simulator/process-table.tsx
+++ app/process-simulator/process-table.tsx
@@ -6,9 +6,9 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-  TableFooter
-} from "@/components/ui/table"
-import { ProcessListItem, snapshotItem } from "./types"
+  TableFooter,
+} from "@/components/ui/table";
+import { ProcessListItem, snapshotItem } from "./types";
 
 const invoices = [
   {
@@ -53,57 +53,50 @@ const invoices = [
     totalAmount: "$300.00",
     paymentMethod: "Credit Card",
   },
-]
+];
 
 interface ProcessTableProps {
-  time: number,
-  snapshots: snapshotItem[]
+  time: number;
+  snapshots: snapshotItem[];
 }
 
-export default function ProcessTable( {time, snapshots}: ProcessTableProps) {
+export default function ProcessTable({ time, snapshots }: ProcessTableProps) {
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px]">Time</TableHead>
 
-          {(Object.keys(snapshots[0]) as (keyof snapshotItem)[]).map(k => {
+          {(Object.keys(snapshots[0]) as (keyof snapshotItem)[]).map((k) => {
             if (k !== "time" && k !== "notes") {
-              return <TableHead key={k} className="font-bold">{ k }</TableHead>
+              return (
+                <TableHead key={k} className="font-bold">
+                  {k.toUpperCase()}
+                </TableHead>
+              );
             }
           })}
 
           <TableHead>Notes</TableHead>
-
         </TableRow>
       </TableHeader>
       <TableBody>
-          {snapshots.map(snapshot => (
-            <TableRow key={snapshot.time}>
-              {
-                (Object.keys(snapshot) as (keyof snapshotItem)[]).map(s => {
-                  return (
-                    <TableCell key={s} className={
-                        (s !== "time" && s !== "notes") ? "font-medium" : ""
-                    }>{snapshot[s]}</TableCell>
-                  )
-                })
-              }
-            </TableRow>
-          ))}
-          <TableRow>
-            {/* <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell> */}
+        {snapshots.map((snapshot) => (
+          <TableRow key={snapshot.time}>
+            {(Object.keys(snapshot) as (keyof snapshotItem)[]).map((s) => {
+              return (
+                <TableCell
+                  key={s}>
+                  <p className={
+                    (s !== "time" && s !== "notes" ? `font-medium text-os-${snapshot[s]?.toLowerCase()}` : "")}>
+                    {snapshot[s]}
+                  </p>
+                </TableCell>
+              );
+            })}
           </TableRow>
+        ))}
       </TableBody>
-      {/* <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter> */}
     </Table>
-  )
+  );
 }
